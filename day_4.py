c0c195bee8c0cb4e8a25f09679a2d62fdce3e16b
White, Evan <evanwht1@gmail.com>
2019-12-04 11:05:02 -0600

    day 4 challenge 2

--- day_4.py
+++ day_4.py
@@ -2,6 +2,23 @@
 __author__ = "evanwht1@gmail.com"
 
 
+class Stack:
+	def __init__(self, v=None):
+		self.data = [] is v == None else [v]
+
+	def top(self):
+		return None if len(self.data) == 0 else self.data[len(self.data)-1]
+
+	def push(self, v):
+		self.data.append(v)
+
+	def size(self):
+		return len(self.data)
+
+	def clear(self, v=None):
+		self.data = [] if v = None else [v]
+
+
 def validate(password):
 	"""
 	Facts about password:
@@ -10,13 +27,18 @@ def validate(password):
 		2 adjacent digits are same
 		always increase in digits when going right
 	"""
+	prev_chars = Stack(password[0])
 	double = False
-	for i in range(len(password)-1):
-		if password[i] > password[i+1]:
+	for i in range(1, len(password)):
+		if prev_chars.top() > password[i]:
 			return False
-		if password[i] == password[i+1]:
-			double = True
-	return double
+		elif prev_chars.top() == password[i]:
+			prev_chars.push(password[i])
+		else:
+			if prev_chars.size() == 2:
+				double = True
+			prev_chars.clear(password[i])
+	return double or prev_chars.size() == 2
 
 
 def main():
@@ -25,8 +47,8 @@ def main():
 		if validate(str(i)):
 			count += 1
 	print(count)
-	return
 
 
 if __name__ == "__main__":
 	main()
+
